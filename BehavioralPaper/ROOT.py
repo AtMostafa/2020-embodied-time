77ab00d9fbf83c6e30d044a5bebb50b1d21da4d4
msafaie <mostafa.safaie@gmail.com>
2019-07-22 16:39:28 +0200

    add ROOT

---
+++ BehavioralPaper/ROOT.py
@@ -0,0 +1,5 @@
+# Add the path to your data folder
+# The data folder is where all the animal folders are: Rat077, Rat078, Rat???, ...
+# Make sure to write the path to your data folder inside the quotation marks, without any extra spaces!
+
+root = r"/data/TimePaper2019/"
